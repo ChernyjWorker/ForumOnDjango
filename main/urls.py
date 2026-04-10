30c466604655bd511c9be666b39b2c04a3a69304
Vladislaw <chernyj.for.work@gmail.com>
2026-04-10 18:04:33 +0300

    add ListView

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from .views import post_list, post_detail, CreatePost
+from .views import PostListView, post_detail, CreatePost
 
 urlpatterns = [
-    path('', post_list, name='post_list'),
+    path('', PostListView.as_view(), name='post_list'),
     path('new/', CreatePost.as_view(), name='post_new'),
     path('/<str:post_slug>', post_detail, name='post_detail'),
 ]
