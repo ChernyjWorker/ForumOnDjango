2ab0aeae6b84b4a5c0cc1eec06a3702fc7f94725
Vladislaw <chernyj.for.work@gmail.com>
2026-04-10 13:29:11 +0300

    regist models and migrations

--- main/admin.py
+++ main/admin.py
@@ -1,16 +1,27 @@
 from django.contrib import admin
-from .models import Post
+from .models import Category, Post
 
 # Register your models here.
 class PostAdmin(admin.ModelAdmin):
     
-    list_display = ('created_at','title','content','updated_at')
-    list_display_links = ('title', 'content')
-    search_fields = ('title', 'content', 'created_at')
+    list_display = ('created_at','title','content','category','updated_at')
+    list_display_links = ('title','content')
+    search_fields = ('title','content','created_at')
     
     
     prepopulated_fields = {
         'slug' : ('title',)
     }
     
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    list_display_links = ('name',)
+    search_fields = ('name',)
+    
+    prepopulated_fields = {
+        'slug': ('name',)
+    }
+    
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Post, PostAdmin)
