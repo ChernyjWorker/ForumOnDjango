117c5d4dc737eb4519316586b9a038384b06b0fa
Vladislaw <chernyj.for.work@gmail.com>
2026-04-14 08:45:42 +0300

    refact CustomUsers model

--- users/models.py
+++ users/models.py
@@ -5,9 +5,17 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class CustomUser(AbstractUser):
-    email = models.EmailField(max_length=66)
+    email = models.EmailField(max_length=66, unique=True)
     avatar = models.ImageField(upload_to='avatars/',blank=True,null=True)
+    description = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now=True)
     
     
     def __str__(self):
         return self.username+'|'+self.email
+    
+    class Meta:
+        ordering = ('-created_at',)
+        verbose_name='Пользователь'
+        verbose_name_plural='Пользователи'
+        
