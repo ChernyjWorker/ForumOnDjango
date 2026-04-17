ed2158ba1cec09623269218cad6bd04dbd7809aa
vladislaw <chernyj.for.work@gmail.com>
2026-04-17 15:53:13 +0300

    add validation in creation user form

--- users/forms.py
+++ users/forms.py
@@ -2,20 +2,35 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import get_user_model, authenticate
 from django.utils.html import strip_tags
-from django.core.validators import RegexValidator
+from django.core.exceptions import ValidationError
 
 User = get_user_model()
 
 
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True, help_text='Обязательное поле')
-    
+    password1 = forms.CharField(required=True, help_text='Введите пароль')
+    password2 = forms.CharField(required=True, help_text='Повторите пароль')
+
+
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+        widgets = {
+            'email':forms.EmailInput(attrs={'class':'form_control','placeholder':'Введите свой email'}),
+            'password1':forms.PasswordInput(attrs={'class':'form_control','placeholder':'Введите пароль'}),
+            'password2':forms.PasswordInput(attrs={'class':'form_control','placeholder':'Повторите пароль'})
+        }
         
     def clean_email(self):
         email = self.cleaned_data['email']
         if User.objects.filter(email=email).exists():
             raise ValidationError('Данный email зарегестрирован!')
         return email
+    
+    def clean_password2(self):
+        password1 = self.cleaned_data['password1']
+        password2 = self.cleaned_data['password2']
+        if password1 and password2 and password1 != password2:
+            raise ValidationError('Пароли не совпадают!')
+        return password2
