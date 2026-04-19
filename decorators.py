03623ffe28d83fbc85d40e5cb117f6b611813ea5
vladislaw <chernyj.for.work@gmail.com>
2026-04-19 16:20:39 +0300

    remove decorators.py

---
+++ decorators.py
@@ -0,0 +1,15 @@
+from functools import wraps
+
+from main.models import Category
+
+
+def navbar_preload(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        categories = Category.objects.all()
+        result = func(*args, **kwargs)
+        result['categories'] = categories
+        return result
+    return wrapper
+
+
